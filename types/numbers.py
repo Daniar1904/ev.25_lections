3e1ff15fcbbc1e6a976123cdc3c2db99e801524b
Daniar1904 <daniarkazakov35@gmail.com>
2022-09-26 20:10:57 +0600

    added1

--- types/numbers.py
+++ types/numbers.py
@@ -90,6 +90,105 @@ from math import pi, sqrt
 # print(math.sqrt(101))
 # print(math.sqrt(6))
 
+# dir() - возвращает методы объекта или функции модуля
+# import math
+# print(dir(math))
+
+# Множественное присваивание
+# a = 5
+# a, b, c = 1, 2, 3
+# v = 5
+# n = 7
+# d = 3
+
+# v, n, d = d, v, n 
+# print(v, n, d)
+
+# num1 = 10
+# num2 = 3
+# num3 = num1
+# num1 = num2
+# num2 = num3
+# print(num1, num2)
+
+# print('hello' * 3) дублирование строк 
+
+# str1 = '12'
+# num1 = 2
+# print(str1 + str(num1))
+
+# Инкремент и дикремент
+
+# Инкремент - увеличение значения какой-либо переменной a = 1 (a += 1 -> a = a + 1)
+
+# a = 0
+# # a +=1 
+# a = a + 1
+# print(a)
+
+# Дикремент - уменьшение значения какой-либо переменной (a -= 1 -> a - 1)
+# a = 0
+# a -= 1
+# print(a)
+
+# * 
+# a = 5
+# a *= a
+# print(a)
+
+#  /
+# a = 10
+# a /= 2
+# print(a)
+
+# a = 7
+# a %= 2
+# print(a)
+
+# id() - возвращает номер ячейки памяти
+# a = 2
+# b = 1
+# print(id(a))
+# print(id(b))
+
+# type() -> выводит тип заданной переменной
+# a = 9
+# b = 'hello'
+# print(type(a))
+# print(type(b))
+
+# var = int(input('Введите число'))
+
+# num1 = int(input('Введите число:'))
+# num2 = int(input('Введите степень:'))
+# print(num1 ** num2)
+
+# Модуль random - предоставляет функции для генерации случайных чисел, букв, символов
+# import random
+
+# print(dir(random))
+
+# num = random.randint(111111, 999999)
+# print(num)
+
+# from random import choice
+
+# import string
+# # print(string.ascii_letters)
+# a = choice(string.ascii_letters)
+# print(a)
+
+
+# import math
+
+# from math import pi
+
+# r = int(input('Введите радиус:'))
+# s = (math.pi * (r ** 2))
+# p = ((2 * r) * math.pi)
+# print(round(s))
+# print(round(p))
+
 
 
 
