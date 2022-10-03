d789d9d04f048307551655449864ca7fba295978
Daniar1904 <daniarkazakov35@gmail.com>
2022-10-03 20:46:50 +0600

    Added ternery

--- repeat/zadacha.py
+++ repeat/zadacha.py
@@ -24,14 +24,27 @@
 # print('Результаты голодных игр:')
 # print(f'Plov: {p}\nManty: {m}\nKuurdak: {k}\nLagman: {l}\nOromo: {o}' )
 
-year = int(input())
-
-result = (
-    'YES' if year % 4 == 0 and 
-    year % 100 != 0 or 
-    year % 400 == 0 else 'NO'
-)
-
-print(result)
+# print("Ноль в качестве знака операции"
+#       "\nзавершит работу программы")
+# while True:
+#     s = input("Знак (+,-,*,/): ")
+#     if s == '0':
+#         break
+#     if s in ('+', '-', '*', '/'):
+#         x = float(input("x="))
+#         y = float(input("y="))
+#         if s == '+':
+#             print("%.2f" % (x+y))
+#         elif s == '-':
+#             print("%.2f" % (x-y))
+#         elif s == '*':
+#             print("%.2f" % (x*y))
+#         elif s == '/':
+#             if y != 0:
+#                 print("%.2f" % (x/y))
+#             else:
+#                 print("Деление на ноль!")
+#     else:
+#         print("Неверный знак операции!")
 
 
